102175104f4e37ba64186bfa3f3e4b1131a08bba
Kang-min Liu <gugod@gugod.org>
2011-11-19 13:39:26 +0800

    apparently VideoOrganizer has nothing to do with video it self

---
+++ lib/FileOrganizer.pm
@@ -0,0 +1,120 @@
+use v5.14;
+
+package FileOrganizer 1.00 {
+    use Moose;
+    use IO::All;
+    use File::Copy qw(move);
+
+    has root => (
+        isa => "Str",
+        is  => "rw",
+        required => 1
+    );
+
+    has collections => (
+        isa => "ArrayRef[FileOrganizer::Collection]",
+        is  => "ro",
+        lazy_build => 1
+    );
+
+    has mess => (
+        isa => "ArrayRef[FileOrganizer::File]",
+        is  => "rw",
+        lazy_build => 1
+    );
+
+    sub _build_collections {
+        my $self = shift;
+
+        my $x = [];
+        for (io($self->root)->all_dirs) {
+            push @$x, FileOrganizer::Collection->new(path => $_->name, organizer => $self)
+                unless $_->filename =~ /^\./;
+        }
+
+        return $x;
+    }
+
+    sub _build_mess {
+        my $self = shift;
+
+        my $x = [];
+        for (io($self->root)->all_files) {
+            push @$x, FileOrganizer::File->new(path => $_->name, organizer => $self)
+                unless $_->filename =~ /^\./;
+        }
+
+        return $x;
+    }
+
+    sub BUILD {
+        my $self = shift;
+
+        unless (-d $self->root) {
+            die $self->root . " is not an directory\n";
+        }
+
+        return $self;
+    }
+
+    sub run {
+        my $self = shift;
+
+        for (@{ $self->mess }) {
+            if ($_->guessed_collection) {
+                move( $_->path, $_->guessed_collection->path )
+            }
+        }
+    }
+};
+
+package FileOrganizer::File 1.00 {
+    use Moose;
+    use File::Basename;
+
+    has organizer => (
+        isa => "FileOrganizer",
+        is  => "ro",
+        required => 1
+    );
+
+    has path => (
+        isa => "Str",
+        is  => "ro",
+        required => 1
+    );
+
+    has name => (
+        isa => "Str",
+        is  => "ro",
+        lazy_build => 1
+    );
+
+    sub _build_name {
+        my $self = shift;
+        return basename($self->path);
+    }
+
+    sub guessed_collection {
+        my $self = shift;
+
+        my @good_collections = map {
+            $_->[0]
+        } sort {
+            length($b->[2]) <=> length($a->[2])
+        } grep {
+            $_->[1] >= $[
+        } map {
+            [$_, index($self->name, $_->name), $_->name]
+        } @{$self->organizer->collections};
+
+        return $good_collections[0];
+    }
+}
+
+package FileOrganizer::Collection 1.00 {
+    use Moose;
+    extends 'FileOrganizer::File';
+};
+
+1;

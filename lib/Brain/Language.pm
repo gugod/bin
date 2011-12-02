651371efe08ccc336f55dee81911a28d18e29a6f
Kang-min Liu <gugod@gugod.org>
2011-12-02 23:33:08 +0800

    split english

--- lib/Brain/Language.pm
+++ lib/Brain/Language.pm
@@ -4,27 +4,28 @@ package Brain::Language {
     use strict;
     use utf8;
     use List::MoreUtils qw(natatime);
+    use Lingua::Sentence;
 
     sub trim {
         my ($self, $text) = @_;
         $text =~ s/^ +//mg;
         $text =~ s/ +$//mg;
-
         return $text;
     }
 
     sub sentences {
         my ($self, $text) = @_;
+        my $splitter = Lingua::Sentence->new('en');
 
         $text = $self->trim($text);
 
         my @result;
-        my $iter = natatime 2, split(/(？\」|。\」|！\」|。(?!\」))/, $text);
+        my $iter = natatime 2, split(/(\x{a0}+|\n\n+|？\」|。\」|！\」|。(?!\」))/, $text);
 
         while (my @vals = $iter->()) {
             local $_ = join "", @vals;
             s/( \A\s+ | \s+\Z )//uxg;
-            push @result, $_;
+            push @result, $splitter->split_array($_);
         }
 
         return @result;

afec85b08910444f072df7ba9f87bdb4d48c86a8
Kang-min Liu <gugod@gugod.org>
2011-12-02 16:12:38 +0800

    trim some excessive whitespace from mobile01

--- lib/Brain/Language.pm
+++ lib/Brain/Language.pm
@@ -5,15 +5,25 @@ package Brain::Language {
     use utf8;
     use List::MoreUtils qw(natatime);
 
+    sub trim {
+        my ($self, $text) = @_;
+        $text =~ s/^ +//mg;
+        $text =~ s/ +$//mg;
+
+        return $text;
+    }
+
     sub sentences {
         my ($self, $text) = @_;
+
+        $text = $self->trim($text);
 
         my @result;
         my $iter = natatime 2, split(/(？\」|。\」|！\」|。(?!\」))/, $text);
 
         while (my @vals = $iter->()) {
             local $_ = join "", @vals;
-            s/( \A\s+ | \s+\Z )//x;
+            s/( \A\s+ | \s+\Z )//uxg;
             push @result, $_;
         }
 

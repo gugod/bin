3ef3bd7b7c39c36a282061936a549b72ef7695c9
Kang-min Liu <gugod@gugod.org>
2011-12-08 15:57:10 +0800

    .

---
+++ lib/HTML/Feature/Engine/HTMLExtractContent.pm
@@ -0,0 +1,26 @@
+package HTML::Feature::Engine::HTMLExtractContent;
+use strict;
+use warnings;
+use HTML::Feature::Result;
+use base qw(HTML::Feature::Base);
+
+use HTML::ExtractContent;
+
+sub run {
+    my $self     = shift;
+    my $html_ref = shift;
+    my $url      = shift;
+    my $result   = shift;
+
+    unless ($result->text) {
+        my $text = HTML::ExtractContent->new->extract($$html_ref)->as_text;
+        if ($text) {
+            $result->text($text);
+            $result->{matched_engine} = "HTMLExtractContent";
+        }
+    }
+
+    return $result;
+}
+
+1;

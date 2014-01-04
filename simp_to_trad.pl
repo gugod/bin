869ab51337792951092f45eb4ac9802ccc5407b9
Kang-min Liu <gugod@gugod.org>
2014-01-04 20:43:07 +0100

    a program that uses Data::HanConvert to convert zh_Hans to zh_Hant

---
+++ simp_to_trad.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/env perl
+use v5.18;
+use strict;
+use warnings;
+
+use Convert::Moji;
+use Data::HanConvert::cn2tw;
+use Data::HanConvert::cn2tw_characters;
+
+sub s2t {
+    state $s2t_converter = Convert::Moji->new([
+        table => {
+            %$Data::HanConvert::cn2tw,
+            %$Data::HanConvert::cn2tw_characters
+        }
+    ]);
+
+
+    return $s2t_converter->convert( $_[0] );
+};
+
+binmode STDIN,  ":utf8";
+binmode STDOUT, ":utf8";
+
+while (<>) {
+    print s2t($_);
+}

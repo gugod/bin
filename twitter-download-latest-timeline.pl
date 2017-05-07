80da12260b6f0bfc41b77e278e549f6dd1332ab7
Kang-min Liu <gugod@gugod.org>
2017-05-07 13:04:58 +0200

    remove retweets, bun includes the retweeted tweets.

--- twitter-download-latest-timeline.pl
+++ twitter-download-latest-timeline.pl
@@ -18,20 +18,38 @@ my $t = Net::Twitter->new( traits   => ['API::RESTv1_1'], %$config );
 my $statuses = $t->home_timeline({ count => 400 });
 my @keep;
 my %users;
+my %seen;
 
 my $datetime_parser = DateTime::Format::Strptime->new(pattern => '%a %b %d %T %z %Y');
 
-my @fields = qw(text lang id in_reply_to_status_id);
-for (@$statuses) {
-    my %s = (
-        'user.id' => $_->{user}{id},
-        'created_at' => $datetime_parser->parse_datetime($_->{created_at})->epoch
-    );
-    @s{@fields} = @{$_}{@fields};
-    push @keep, \%s;
+my @source;
+push @source, @$statuses;
 
-    $users{ $_->{user}{id} } //= $_->{user};
+my @fields = qw(text lang id in_reply_to_status_id);
+while (my $tweet = shift @source) {
+    next if $seen{$tweet->{id}}++;
+
+    if ($tweet->{retweeted_status}) {
+        push @source, $tweet->{retweeted_status};
+        next;
+    }
+
+    my %s = (
+        'user.id' => $tweet->{user}{id},
+        'created_at' => $datetime_parser->parse_datetime($tweet->{created_at})->epoch
+    );
+
+    for my $field (@fields) {
+        if (defined($tweet->{$field})) {
+            $s{$field} = $tweet->{$field};
+        }
+    }
+
+    $users{ $tweet->{user}{id} } //= $tweet->{user};
+
+    push @keep, \%s;
 }
+
 
 my $srl = Sereal::Encoder->new();
 my $ts = time;
@@ -39,6 +57,11 @@ open my $fh, ">", "${output_dir}/twitter-timeline-${ts}.srl";
 print $fh "". $srl->encode(\@keep);
 close($fh);
 
-open my $fh, ">", "${output_dir}/twitter-timeline-users-${ts}.srl";
+open $fh, ">", "${output_dir}/twitter-timeline-users-${ts}.srl";
 print $fh "". $srl->encode(\%users);
 close($fh);
+
+open $fh, ">", "${output_dir}/twitter-timeline-FULL-${ts}.srl";
+print $fh "". $srl->encode($statuses);
+close($fh);
+

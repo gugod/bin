82fa5af539297f0347ce6eedaebd2ea4d88da9da
Kang-min Liu <gugod@gugod.org>
2017-06-12 22:52:29 +0200

    make a sub for the extraction logic.

--- get-mjpeg.pl
+++ get-mjpeg.pl
@@ -24,6 +24,46 @@ sub on_mime_part {
     say "$ofh <= $content_type $content_length";
 }
 
+sub extract_one_part_maybe {
+    my ($response_content, $boundary, $output_dir) = @_;
+    my $error;
+
+    if ($response_content =~ /\A\s*$boundary/x) {
+        my $head_pos = index($response_content, "${CRLF}${CRLF}");
+        my $head = substr($response_content, length($boundary), $head_pos - length($boundary));
+        my @headers = map { split(/:\s*/, $_, 2) } split(/${CRLF}/, $head);
+
+        if (@headers % 2 == 1 && $headers[0] =~ /\A--/) {
+            shift @headers;
+        }
+
+        my ($content_type, $content_length);
+        for (my $i = 0; $i < @headers; $i += 2) {
+            my $field = lc($headers[$i]);
+            my $value = $headers[$i+1];
+            if ( (!defined($content_type)) && ($field eq 'content-type') ) {
+                $content_type = $value;
+            }
+            if ( (!defined($content_length)) && ($field eq 'content-length') ) {
+                $content_length = $value;
+            }
+        }
+
+        if (length($response_content) >= length($boundary) + length($head) + 4 + $content_length) {
+            my $part = substr($response_content, $head_pos + 4, $content_length);
+            on_mime_part($content_type, $content_length, $part, $output_dir);
+            substr($response_content, 0, $head_pos + 4 + $content_length) = "";
+            while (index($response_content, $CRLF) == 0) {
+                substr($response_content, 0, length($CRLF)) = "";
+            }
+        }
+    } else {
+        $error = "Missing mime boundary <$boundary>\n" . (substr($response_content, 0, 140) =~ s/\P{ascii}/./r) . "--\n";
+    }
+
+    return ($response_content, $error);
+}
+
 local $|;
 
 my %opts;
@@ -33,7 +73,7 @@ GetOptions(
 );
 
 my $url = shift(@ARGV) or die "Requires an URL";
-my ($boundary, $boundary_line);
+my $boundary;
 my $response_content = "";
 my $response_part = "";
 my $output_dir = $opts{o} // $opts{output};
@@ -56,9 +96,9 @@ $ua->add_handler(
         my $ct = $response->header("Content-Type");
         ($boundary) = $ct =~ m{multipart/x-mixed-replace; boundary=(.+)$};
         die "No boundary separator" unless $boundary;
-        $boundary_line = $boundary;
-        # $boundary_line = "${boundary}${CRLF}";
-        # $boundary_line =~ s/^----/--/;
+        if ($boundary !~ /\A--/) {
+            $boundary = "--$boundary";
+        }
         return 1;
     }
 );
@@ -68,39 +108,10 @@ $ua->add_handler(
 
         $response_content .= $chunk;
 
-        if ($response_content =~ /\A\s*${boundary_line}/x) {
-            my $head_pos = index($response_content, "${CRLF}${CRLF}");
-            my $head = substr($response_content, length($boundary_line), $head_pos - length($boundary_line));
-            my @headers = map { split(/:\s*/, $_, 2) } split(/${CRLF}/, $head);
-
-            if (@headers % 2 == 1 && $headers[0] =~ /\A--/) {
-                shift @headers;
-            }
-
-            my ($content_type, $content_length);
-            for (my $i = 0; $i < @headers; $i += 2) {
-                my $field = lc($headers[$i]);
-                my $value = $headers[$i+1];
-                if ( (!defined($content_type)) && ($field eq 'content-type') ) {
-                    $content_type = $value;
-                }
-                if ( (!defined($content_length)) && ($field eq 'content-length') ) {
-                    $content_length = $value;
-                }
-            }
-
-            if (length($response_content) >= length($boundary_line) + length($head) + 4 + $content_length) {
-                my $part = substr($response_content, $head_pos + 4, $content_length);
-                on_mime_part($content_type, $content_length, $part, $output_dir);
-                substr($response_content, 0, $head_pos + 4 + $content_length) = "";
-                while (index($response_content, $CRLF) == 0) {
-                    substr($response_content, 0, length($CRLF)) = "";
-                }
-            }
-        } else {
-            die "Missing mime boundary <$boundary>\n" . (substr($response_content, 0, 140) =~ s/\P{ascii}/./r) . "--\n";
+        ($response_content, my $error) = extract_one_part_maybe($response_content, $boundary, $output_dir);
+        if ($error) {
+            die $error;
         }
-
         return 1;
     }
 );
@@ -109,3 +120,7 @@ my $died = $res->header("X-Died");
 if ($died) {
     say "ERR: $died";
 }
+
+__END__
+
+DEBUG: http://210.241.63.120/abs2mjpg/bmjpg?camera=264&1497299198151

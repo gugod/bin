e6ffb78b53641ac7c4a21d578b6bbc29b10c1762
Kang-min Liu <gugod@gugod.org>
2017-06-12 21:42:09 +0200

    get it working with http://210.242.136.66/T61-73K+800

--- get-mjpeg.pl
+++ get-mjpeg.pl
@@ -1,6 +1,9 @@
 #!/usr/bin/env perl
 use v5.18;
 use strict;
+use warnings;
+
+use Carp::Always;
 
 use constant CRLF => "\015\012";
 my $CRLF = CRLF();
@@ -24,12 +27,12 @@ sub on_mime_part {
 local $|;
 
 my %opts;
-GetOptios(
+GetOptions(
     \%opts,
     "o|output=s",
 );
 
-my $url = shift(@ARGV) or die;
+my $url = shift(@ARGV) or die "Requires an URL";
 my ($boundary, $boundary_line);
 my $response_content = "";
 my $response_part = "";
@@ -42,7 +45,10 @@ unless (-d $output_dir) {
     make_path($output_dir);
 }
 
-my $ua = LWP::UserAgent->new;
+my $ua = LWP::UserAgent->new(
+    timeout => 60,
+);
+
 $ua->add_handler(
     response_header =>  sub {
         my ($response, $ua, $h) = @_;
@@ -50,24 +56,26 @@ $ua->add_handler(
         my $ct = $response->header("Content-Type");
         ($boundary) = $ct =~ m{multipart/x-mixed-replace; boundary=(.+)$};
         die "No boundary separator" unless $boundary;
-
-        $boundary_line = "--${boundary}${CRLF}";
-        $boundary_line =~ s/^----/--/;
+        $boundary_line = $boundary;
+        # $boundary_line = "${boundary}${CRLF}";
+        # $boundary_line =~ s/^----/--/;
+        return 1;
     }
 );
-$ua->get(
-    $url,
-    ':read_size_init' => 10240,
-    ':content_cb' => sub {
-        my ($chunk, $response, $proto) = @_;
+$ua->add_handler(
+    response_data => sub {
+        my ($response, $ua, $h, $chunk) = @_;
 
         $response_content .= $chunk;
-        my $pos = index($response_content, $boundary_line);
-        if ($pos == 0) {
+
+        if ($response_content =~ /\A\s+${boundary_line}/x) {
             my $head_pos = index($response_content, "${CRLF}${CRLF}");
             my $head = substr($response_content, length($boundary_line), $head_pos - length($boundary_line));
+            my @headers = map { split(/:\s*/, $_, 2) } split(/${CRLF}/, $head);
 
-            my @headers = map { split(/:/, $_, 2) } split(/${CRLF}/, $head);
+            if (@headers % 2 == 1 && $headers[0] =~ /\A--/) {
+                shift @headers;
+            }
 
             my ($content_type, $content_length);
             for (my $i = 0; $i < @headers; $i += 2) {
@@ -83,7 +91,7 @@ $ua->get(
 
             if (length($response_content) >= length($boundary_line) + length($head) + 4 + $content_length) {
                 my $part = substr($response_content, $head_pos + 4, $content_length);
-                on_mime_part($content_type, $content_length, $part);
+                on_mime_part($content_type, $content_length, $part, $output_dir);
                 substr($response_content, 0, $head_pos + 4 + $content_length) = "";
                 while (index($response_content, $CRLF) == 0) {
                     substr($response_content, 0, length($CRLF)) = "";
@@ -92,5 +100,12 @@ $ua->get(
         } else {
             die "Missing mime boundary <$boundary>\n" . "--\n" . (substr($response_content, 0, 140) =~ s/\P{ascii}/./r) . "--\n";
         }
+
         return 1;
-});
+    }
+);
+my $res = $ua->get($url);
+my $died = $res->headers("X-Died");
+if ($died) {
+    say "ERR: $died";
+}

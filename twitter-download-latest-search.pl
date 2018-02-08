bd47efef12d5495cd15ad38a967be3defc44bc0d
Kang-min Liu <gugod@gugod.org>
2018-02-08 21:31:27 +0900

    take search terms from @ARGV

--- twitter-download-latest-search.pl
+++ twitter-download-latest-search.pl
@@ -5,36 +5,32 @@ use warnings;
 
 use Net::Twitter;
 use YAML;
-use Sereal::Encoder;
-use DateTime;
-use DateTime::Format::Strptime;
+use Getopt::Long 'GetOptions';
 
 use FindBin;
 use lib $FindBin::Bin . "/lib";
 use Fun::File qw(srl_slurp srl_spew);
 
-my %args = @ARGV;
-my $config_file = $args{'-c'} or die;
-my $output_dir  = $args{'-o'} or die;
+my %opts;
+GetOptions(
+    \%opts,
+    "c=s",
+    "o=s",
+);
+
+my @search_terms = @ARGV;
+my $config_file = $opts{c} or die;
+my $output_dir  = $opts{o} or die;
 
 my $config = YAML::LoadFile($config_file);
 
 my $t = Net::Twitter->new( traits   => ['API::RESTv1_1'], %$config );
 
-use utf8;
 binmode STDOUT, ":utf8";
 
-my @search_terms = (
-    "台灣 地震",
-    "台湾 地震",
-    "taiwan earthquake",
-    "taiwan tremblement de terre",
-    "taiwan séisme",
-    "aardbeving taiwan",
-    "землетрясение Тайване",
-);
-
 for (@search_terms) {
+    utf8::decode($_);
+
     my $res = $t->search({ q => $_, count => 100 });
     my $ts = time;
     srl_spew("${output_dir}/twitter-raw-search-${ts}.srl", $res);

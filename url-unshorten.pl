74e3eb446152846dba8f4e93aef7fa8bb14132ef
Kang-min Liu <gugod@gugod.org>
2018-03-01 00:32:34 +0900

    program to effectively un-shorten some of the URL from input.

---
+++ url-unshorten.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/env perl
+use v5.18;
+use strict;
+use warnings;
+
+use FindBin;
+use lib $FindBin::Bin . "/lib";
+use Fun::Web qw(url_remove_tracking_params url_unshorten);
+
+while(<>) {
+    chomp;
+    my $url = $_;
+    next unless URI->new($url)->path;
+    my $url2 = url_remove_tracking_params( url_unshorten($url) );
+    say $url2 if URI->new($url2)->path;
+}

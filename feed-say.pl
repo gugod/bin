8ee0a54f6a00a5f79e0e54216293968b8578b100
Kang-min Liu <gugod@gugod.org>
2018-07-24 09:25:01 +0900

    insert a FULL STOP betwen two Han characters.

--- feed-say.pl
+++ feed-say.pl
@@ -9,11 +9,15 @@ use URI;
 use XML::Feed;
 use Getopt::Long;
 
+sub pick {
+    $_[ rand(@_) ]
+}
+
 sub print_and_tts {
     my ($str, $file) = @_;
     my $voice;
     if ($str =~ /^\p{ASCII}+$/) {
-        $voice = random("Daniel", "Jill");
+        $voice = pick("Daniel", "Samantha");
     } elsif ($str =~ /(\p{Hiragana}|\p{Katakana})/) {
         $voice = "Kyoko";
     } elsif ($str =~ /\p{Han}/) {
@@ -31,14 +35,16 @@ GetOptions(
     \%opts,
     "o",
 );
-my @feeds = map { chomp; XML::Feed->parse(URI->new($_)) } @ARGV;
 
 my $striper = HTML::Strip->new;
-
 my $i = "0000";
 my $file;
 binmode STDOUT, ':utf8';
-for my $feed (@feeds) {
+
+my @feed_uri = @ARGV;
+for (@feed_uri) {
+    my $feed = XML::Feed->parse(URI->new($_)) or next;
+
     if ($opts{o} && -d $opts{o}) {
         $file = $opts{o} . "/feed_$i.m4a"; $i++;
     }
@@ -60,6 +66,7 @@ for my $feed (@feeds) {
         $msg =~ s/$RE{URI}/ /g;
         $msg =~ s/[\p{Punct}\p{Space}]{2,}/ /g;
         $msg =~ s/\s{2,}/\N{IDEOGRAPHIC FULL STOP}/g;
+        $msg =~ s/(\p{Han}) (\p{Han})/$1\N{IDEOGRAPHIC FULL STOP}$2/g;
         $msg =~ s/\p{Space}+\z//;
         $msg =~ s/\A\p{Space}+//;
         next if $msg =~ /\A\p{Space}*\z/;

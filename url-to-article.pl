59e4e35732a7f8fce2a6cc23ddc38b973a821a7a
Kang-min Liu <gugod@gugod.org>
2018-10-03 16:10:43 +0900

    make them play together

--- url-to-article.pl
+++ url-to-article.pl
@@ -7,13 +7,13 @@ use Mojo::DOM;
 use Encode qw(encode_utf8 decode_utf8);
 use HTML::ExtractContent;
 use JSON qw(encode_json);
-use Digest::MD5 qw(md5_hex);
 use Getopt::Long qw(GetOptions);
-use List::Util qw(uniqstr);
+use MCE::Stream;
 
 sub extract_title {
     my ($dom) = @_;
     my $title = $dom->find("title");
+    return unless $title->[0];
     $title = $title->[0]->text;
     return $title;
 }
@@ -44,77 +44,71 @@ GetOptions(
 
 die "-o <DIR>" unless $opts{o} || (! -d $opts{o});
 
-open my $pho, ">", $opts{o} . "/json-feed-".timestamp().".json" or die $!;
-open my $log, ">", $opts{o} . "/json-feed-".timestamp().".log"  or die $!;
+my @links = map { chomp; $_ } <>;
+
+my $log_fn = $opts{o} . "/json-feed-".timestamp().".log";
 
 my $firefox = Firefox::Marionette->new(
     (($opts{verbose}) ? (visible => 1):()),
-    page_load => 10_000, # 10s
-);
-my @links;
-
-my %json_feed = (
-    version => "https://jsonfeed.org/version/1",
-    title => "Articles",
-    items => [],
 );
 
-while(<>) {
-    chomp;
-    my $uri = URI->new($_);
-    my $err;
-    eval {
-        $firefox->go($uri);
-        1;
-    } or do {
-        $err = $@;
-    };
-    if ($err) {
-        say $log "SRCERR: $uri $err";
-        next;
+mce_stream {
+    input_data => sub {
+        my $uri = pop(@links) or return;
+
+        my $html;
+        MCE->say("URI: $uri");
+
+        eval {
+            $firefox->go($uri);
+            $html = "" . $firefox->html;
+            1;
+        } or do {
+            $html = '';
+        };
+
+        return [ $uri, $html ];
     }
+}, sub {
+    my ($uri, $html) = @$_;
 
-    my $html = $firefox->html;
+    return unless $html;
+
     $html = decode_utf8($html) unless Encode::is_utf8($html);
     my $dom = Mojo::DOM->new($html);
-    for my $e ($dom->find('a[href]')->each) {
-        my $href = $e->attr("href");
-        my $u = URI->new_abs($href, $uri);
-        if ($u->scheme =~ /^http/ && $u->host eq $uri->host) {
-            push @links, $u;
-            say $log "URL: $u";
-        }
-    }
-}
 
-if (@links) {
-    @links = uniqstr(@links);
-
-    while (my $uri = pop(@links)) {
-        $firefox->go($uri);
-        my $html = $firefox->html;
-        $html = decode_utf8($html) unless Encode::is_utf8($html);
-        my $dom = Mojo::DOM->new($html);
-
-        my $title = extract_title($dom);
-        my $content = extract_text_content($html);
-
-        if ($content) {
-            push @{ $json_feed{items} }, {
-                id => md5_hex(encode_utf8($uri . $title . $content)),
+    my $title = extract_title($dom);
+    if (!$title) {
+        MCE->sendto(
+            file => encode_json({
                 url => "$uri",
-                title => $title,
-                content_text => $content,
-            };
-            say $log "ITEM: " . encode_json({
-                url => "$uri",
-                title => $title,
-                content_text => $content,
-            });
-        } else {
-            say $log "NO CONTENT: $uri" ;
-        }
+                error => "NO TITLE",
+            })."\n",
+            $log_fn
+        );
+        return;
     }
 
-    say $pho encode_json(\%json_feed);
-}
+    my $content = extract_text_content($html);
+
+    if (!$content) {
+        MCE->sendto(
+            file => encode_json({
+                url => "$uri",
+                error => "NO CONTENT",
+            })."\n",
+            $log_fn
+        );
+        return;
+    }
+
+    MCE->sendto(
+        file => encode_json({
+            url => "$uri",
+            title => $title,
+            content_text => $content,
+        })."\n",
+        $log_fn
+    );
+    return;
+};

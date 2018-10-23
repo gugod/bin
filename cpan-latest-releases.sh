180686b1e374ab4a07ca73ad9afc963f1d03b1df
Kang-min Liu <gugod@gugod.org>
2018-10-23 13:17:35 +0900

    leave only the parts start from AUTHOR id. (cpanm can already take these as its parameter.)

--- cpan-latest-releases.sh
+++ cpan-latest-releases.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-curl --silent 'https://mirror.leaseweb.com/CPAN/modules/01modules.mtime.rss' | ack --output '$1' '<link>(.+?[^/]+)</link>' | headskip -1
+curl --silent 'https://mirror.leaseweb.com/CPAN/modules/01modules.mtime.rss' | ack --output '$1' '<link>(.+?[^/]+)</link>' | headskip -1 | cut -c 48-

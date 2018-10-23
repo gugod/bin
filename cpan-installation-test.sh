9a78869335b633b84e2fe53c19c79cc37baf34d0
Kang-min Liu <gugod@gugod.org>
2018-10-23 13:11:36 +0900

    store all output

--- cpan-installation-test.sh
+++ cpan-installation-test.sh
@@ -1,47 +1,46 @@
 #!/bin/bash
 
 PERL_INSTALLATION=v28
-LOGDIR=~/var/log/cpan-installation-test
-
-PERL_CPANM_OPT=${PERL_CPANM_OPT:-}
-PERL_CPANM_OPT=${PERL_CPANM_OPT/--notest//}
-echo PERL_CPANM_OPT=$PERL_CPANM_OPT
+LOGDIR=~/var/cpan-installation-test
 
 if [[ ! -d $LOGDIR ]]; then mkdir -p $LOGDIR; fi
 
 eval "$(perlbrew init-in-bash)"
 perlbrew use ${PERL_INSTALLATION}
-perlbrew list-modules | grep -v '^Perl$' | cpanm --uninstall -f
-
-echo "Perlbrew"
-echo "========"
-perlbrew info
-echo "========"
-
 function test_one_dist {
+
     local dist=$1
     local distdir=$(echo $dist | perl -p -e 's/[^0-9A-Za-z\.]+/-/gi')
     local lib_name="${PERL_INSTALLATION}@cpan_installation_test_${RANDOM}"
 
-    echo ">>> Use ${lib_name} for $dist";
-    perlbrew lib create $lib_name
-    perlbrew use $lib_name
+    perlbrew list-modules | grep -v '^Perl$' | cpanm --uninstall -f
 
-    echo "--- cpanm $dist";
-    cpanm --verbose $dist > $LOGDIR/${distdir}-cpanm.log 2>&1
-    echo "--- done"
+    (
+        echo "Perlbrew"
+        echo "========"
+        perlbrew info
+        echo "========"
 
-    rc=$?
-    if [[ $rc -eq 0 ]]; then
-        touch $LOGDIR/${distdir}-cpanm.ok
-    else
-        touch $LOGDIR/${distdir}-cpanm.fail
-    fi
+        echo ">>> Use ${lib_name} for $dist";
+        perlbrew lib create $lib_name
+        perlbrew use $lib_name
 
-    perlbrew list-modules > $LOGDIR/${distdir}-installed.log
-    perlbrew use ${PERL_INSTALLATION}
-    perlbrew lib delete $lib_name
-    echo "<<< DELETED ${lib_name}";
+        echo "--- cpanm $dist";
+        cpanm --verbose $dist
+        echo "--- done"
+
+        rc=$?
+        if [[ $rc -eq 0 ]]; then
+            touch $LOGDIR/${distdir}-cpanm.ok
+        else
+            touch $LOGDIR/${distdir}-cpanm.fail
+        fi
+
+        perlbrew list-modules > $LOGDIR/${distdir}-installed.log
+        perlbrew use ${PERL_INSTALLATION}
+        perlbrew lib delete $lib_name
+        echo "<<< DELETED ${lib_name}";
+    ) > $LOGDIR/${distdir}.log 2>&1
 }
 
 # MODULES=$(echo Elastijk App::csv2tsv yagg URI::Fast Test::Locus grepmail)

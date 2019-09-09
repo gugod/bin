63b5160da010e8957b2b7d58919ed0e0df289bdf
Kang-min Liu <gugod@gugod.org>
2019-09-09 11:37:46 +0900

    sort the final output by str length.

--- solve-longest-english-pokemon-name-sequence.pl
+++ solve-longest-english-pokemon-name-sequence.pl
@@ -39,6 +39,9 @@ sub solve {
 
 my $longest_sequences = solve([qw<audino bagon baltoy banette bidoof braviary bronzor carracosta charmeleon cresselia croagunk darmanitan deino emboar emolga exeggcute gabite girafarig gulpin haxorus heatmor heatran ivysaur jellicent jumpluff kangaskhan kricketune landorus ledyba loudred lumineon lunatone machamp magnezone mamoswine nosepass petilil pidgeotto pikachu pinsir poliwrath poochyena porygon2 porygonz registeel relicanth remoraid rufflet sableye scolipede scrafty seaking sealeo silcoon simisear snivy snorlax spoink starly tirtouga trapinch treecko tyrogue vigoroth vulpix wailord wartortle whismur wingull yamask>]);
 
-for my $seq (@$longest_sequences) {
-    say join " ", @$seq;
+my @strs = sort { length($a) <=> length($b) } map { join(" ", @$_) } @$longest_sequences;
+
+say "Done: ";
+for my $s (@strs) {
+    say "- $s";
 }

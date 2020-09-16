bc33c24ae80f5b68558c3e2b86ff39a60deb213e
Kang-min Liu <gugod@gugod.org>
2020-09-17 08:59:18 +0900

    .

--- provision-macOS.sh
+++ provision-macOS.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 brew install ack gnupg mosh w3m urlview tmux ripgrep syncthing kpcli darkhttpd 
-brew cask install emacs firefox karabiner-elements font-terminus font-symbola font-jf-open-huninn
+brew cask install emacs firefox karabiner-elements font-terminus font-symbola font-jf-open-huninn font-gensenrounded

87e4d59f0277076793c3341d7aba4b0ea822eb68
Kang-min Liu <gugod@gugod.org>
2022-02-02 09:37:23 +0900

    +git-annex

--- provision-macOS.sh
+++ provision-macOS.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-brew install ack gnupg mosh w3m urlview tmux ripgrep syncthing kpcli darkhttpd fzf
+brew install ack gnupg mosh w3m urlview tmux ripgrep syncthing kpcli darkhttpd fzf git-annex
 brew cask install emacs firefox karabiner-elements font-terminus font-symbola font-jf-open-huninn font-gensenrounded

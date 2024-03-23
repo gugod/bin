938c4bf95b0c144c3b6bab92749ab19791c38698
Kang-min Liu <gugod@gugod.org>
2024-03-23 10:48:51 +0900

    add a bunch of packages.

--- provision-archlinux.sh
+++ provision-archlinux.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pacman -S which git vim emacs cmake make clang libtool stow fossil openssh
+pacman -S which git vim emacs cmake make clang libtool stow fossil openssh pass xclip msmtp isync ripgrep zopfli yt-dlp git-annex

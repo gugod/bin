14ee9ff95733db834fe45217f35ae3fc91cfc457
Kang-min Liu <gugod@gugod.org>
2024-03-26 15:23:50 +0900

    scripts for disabling as many animation effects as possible.

--- provision-macOS.sh
+++ provision-macOS.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
 
+defaults write -g NSScrollViewRubberbanding -int 0
+defaults write -g NSAutomaticWindowAnimationsEnabled -bool false
+defaults write -g NSScrollAnimationEnabled -bool false
+defaults write -g NSWindowResizeTime -float 0.001
+defaults write -g QLPanelAnimationDuration -float 0
+defaults write -g NSScrollViewRubberbanding -bool false
+defaults write -g NSDocumentRevisionsWindowTransformAnimation -bool false
+defaults write -g NSToolbarFullScreenAnimationDuration -float 0
+defaults write -g NSBrowserColumnAnimationSpeedMultiplier -float 0
+defaults write com.apple.dock autohide-time-modifier -float 0
+defaults write com.apple.dock autohide-delay -float 0
+defaults write com.apple.dock expose-animation-duration -float 0
+defaults write com.apple.dock springboard-show-duration -float 0
+defaults write com.apple.dock springboard-hide-duration -float 0
+defaults write com.apple.dock springboard-page-duration -float 0
+defaults write com.apple.finder DisableAllAnimations -bool true
+defaults write com.apple.Mail DisableSendAnimations -bool true
+defaults write com.apple.Mail DisableReplyAnimations -bool true
+defaults write NSGlobalDomain NSWindowResizeTime .001
+defaults write com.apple.dock expose-animation-duration -int 0
+killall Dock
+
 brew install ack gnupg mosh w3m urlview tmux ripgrep syncthing kpcli darkhttpd fzf git-annex
-brew cask install emacs firefox karabiner-elements font-terminus font-symbola font-jf-open-huninn font-gensenrounded
+brew cask --cask emacs firefox karabiner-elements font-terminus font-symbola font-jf-open-huninn font-gensenrounded
